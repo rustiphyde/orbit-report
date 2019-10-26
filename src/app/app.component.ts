cbb6325a7f25379997aa605cdd51225d582809a0
rustiphyde <rustiphyde@gmail.com>
2019-10-25 19:31:19 -0500

    Add Sorting and Searching

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,39 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report-project';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
+    this.displayList = []
     this.sourceList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
-    window.fetch(satellitesUrl).then(function(response) {
-       response.json().then(function(data) {
+    window.fetch(satellitesUrl).then(function (response) {
+      response.json().then(function (data) {
 
-          let fetchedSatellites = data.satellites;
-         for (let i = 0; i < fetchedSatellites.length; i++){
-           let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational)
-           this.sourceList.push(satellite);
-         }
-
-       }.bind(this));
+        let fetchedSatellites = data.satellites;
+        for (let i = 0; i < fetchedSatellites.length; i++) {
+          let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational)
+          this.sourceList.push(satellite);
+        }
+        // make a copy of the sourceList to be shown to the user
+      this.displayList = this.sourceList.slice(0);
+      }.bind(this));
     }.bind(this));
 
- }
+  }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for (let i = 0; i < this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+      }
+    }
+    // assign this.displayList to be the the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
+  }
 }
