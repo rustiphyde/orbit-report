3b4413796a022b94556217b9b992e7b77b12fafd
rustiphyde <rustiphyde@gmail.com>
2019-10-25 20:56:27 -0500

    Add Zebra Stripe and Count Component Bonus

---
+++ src/app/satellite.ts
@@ -0,0 +1,22 @@
+export class Satellite {
+name: string;
+orbitType: string;
+type: string;
+operational: boolean;
+launchDate: string;
+  constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+    this.name = name;
+    this.orbitType = orbitType;
+    this.type = type;
+    this.operational = operational;
+    this.launchDate = launchDate;
+  }
+
+  shouldShowWarning(): boolean {
+    return this.type.toLowerCase() === 'space debris';
+  }
+
+  zebraStripe(arr): boolean {
+    return arr.indexOf(this) % 2 === 0 && this.type.toLowerCase() !== 'space debris';
+  }
+  }
